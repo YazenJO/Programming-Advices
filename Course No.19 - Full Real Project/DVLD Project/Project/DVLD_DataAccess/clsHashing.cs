aa6080d20632357a69a54a8bf973dee16b265c9e
Yazen AbuSharkh <yazenbilal2005@gmail.com>
2025-07-25 18:32:47 +0300

    Second

---
+++ Course No.19 - Full Real Project/DVLD Project/Project/DVLD_DataAccess/clsHashing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DVLD.Classes
+{
+    public class clsHashing
+    {   
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array
+                byte[] bytes = sha256Hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                // Convert byte array to a string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            string hashOfInput = HashPassword(password);
+            return StringComparer.OrdinalIgnoreCase.Compare(hashOfInput, hashedPassword) == 0;
+        }
+        
+    }
+}
